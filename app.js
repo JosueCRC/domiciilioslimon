d44c8ae9ea68e5b331189330158335f9fc181491
Josue Gonzalez <39922816+JosueCRC@users.noreply.github.com>
2020-06-08 10:08:43 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -32,10 +32,9 @@ function ingreso(){
         var errorCode = error.code;
         var errorMessage = error.message;
         // ...
-        console.log(errorCode);
-        console.log(errorMessage);
+        alert(errorCode);
+        alert(errorMessage);
       });
-   
 }
 
 function observador(){
@@ -83,19 +82,14 @@ function aparece(user){
     var user = user;
     var contenido = document.getElementById('contenido');
     if (user.emailVerified){
-        contenido.innerHTML= `
+        contenido.innerHTML= //html
+        `
           <div class="alert alert-light" role="alert">
-            
-           
-
             <div class="card-header card-header-primary text-center">
-
                 <div class="row">
                   <div class="col-sm-8"></div>
                   <div class="col-sm-4" ><p id="username">${user.email} </p>
-                  
-                  <a href="#" class="badge badge-danger "data-toggle="modal" data-target="#cerrarSesion">x cerrar sesion</a>
-                  
+                  <a href="#" class="badge badge-danger "data-toggle="modal" data-target="#" onclick="cerrar()">x cerrar sesion</a>
                 </button>
                 
                 <!-- Modal -->
@@ -113,7 +107,7 @@ function aparece(user){
                       </div>
                       <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-                        <button  class="btn btn-danger" onclick="cerrar()">Cerrar sesion</button>
+                        <button  class="btn btn-danger" >Cerrar sesion</button>
                       </div>
                     </div>
                   </div>
@@ -123,11 +117,8 @@ function aparece(user){
 
                     <h3 class="card-title" id="titulo">Area de Salud Limon.!</h3>
               <p class="description text-center" id="encabezado">FORMULARIO DE REGISTRO PARA EL ENVIO A DOMICILIO DE LOS MEDICAMENTOS</p>
-              <div class="social-line">
-           
-              
+              <div class="social-line">             
               </div>
-
               <hr>
               <div class="container">
       <div class="card-body">
@@ -166,7 +157,7 @@ function aparece(user){
                     <i class="material-icons">face</i>
                   </span>
                 </div>
-                <input type="text" id="nombre" placeholder="Nombre del Paciente" value="" class="form-control" />
+                <input type="text" id="nombre" placeholder="Nombre del Paciente" value="" class="form-control text-uppercase" />
               </div>
             </div>
           </div>
@@ -239,7 +230,7 @@ function aparece(user){
             
               <div class="form-group">
               <label for="exampleFormControlTextarea1">Señas Exactas:</label>
-              <textarea class="form-control" id="direccion" rows="2"></textarea>
+              <textarea class="form-control text-uppercase" id="direccion" rows="2"></textarea>
             </div>
               <div class="row">
                 <div class="col-sm">
@@ -263,12 +254,76 @@ function aparece(user){
     </div>
             </div>
 
-            
+            <div class="container mt-4" >
+            <div class="col-4 col-md-6 sx-12">
+              <div class="input-group">
+              
+              <input type="text" id="myInput" onkeyup="myFiltro()" placeholder="Buscar" value="" class="form-control" />
+              <span class="input-group-text">
+                <i class="material-icons">search</i>
+              </span>
+            </div>
+          </div>
+      </div>
      
+    <div class="container mt-3 " >     
+    <div class="table-responsive">
+      <table class="table" >
+        <thead>
+          <tr>
+            <th scope="col">TipoID</th>
+            <th scope="col">Cedula</th>
+            <th scope="col">Nombre Completo</th>
+            <th scope="col">Ebais</th>
+            <th scope="col">Telefono</th>
+            <th scope="col">Fecha</th>   
+            <th>Eliminar</th>
+            <th>Editar</th>
+            <th>Imprimir</th>
+          </tr>
+        </thead>
+        <tbody id="tabla"> </tbody>
+      </table>
+    </div>
+  </div>
             
         
-    `;
-    }
+    `
+   //Leer documentos.
+var tabla = document.getElementById('tabla');
+db.collection("CopiasTelefono").orderBy("Fecha", "desc").onSnapshot((querySnapshot) => {
+//db.collection("CopiasTelefono").where("Cedula", "==", "").onSnapshot((querySnapshot) => {
+    tabla.innerHTML='';
+    contador = 0
+     querySnapshot.forEach((doc) => {
+        /* console.log(`${doc.id} => ${doc.data()}`); */
+        contador++;
+        
+        //console.log (contador);
+        if (user.emailVerified){
+
+        }else{
+            
+        }
+            
+
+
+        tabla.innerHTML += `
+        <tr>
+        <td>${doc.data().TipoID}</td>
+        <th scope="row">${doc.data().Cedula}</th>
+        <td>${doc.data().Nombre}</td>
+        <td>${doc.data().Ebais}</td>
+        <td>${doc.data().Telefono}</td>
+        <td>${doc.data().Fecha}</td>
+        <td><button class="btn btn-danger btn-just-icon btn-sm" onclick="eliminar('${doc.id}')"> <i class="material-icons">close</i></button></td>
+        <td><button class="btn btn-success btn-just-icon btn-sm"onclick="editar('${doc.id}','${doc.data().TipoID}','${doc.data().Cedula}','${doc.data().Nombre}','${doc.data().Telefono}','${doc.data().Ebais}','${doc.data().Correo}','${doc.data().Direccion}','${doc.data().Recibe}','${doc.data().Fecha}')">  <i class="material-icons">edit</i></button></td>
+        <td><button class="btn btn-warning btn-just-icon btn-sm"onclick="imprimirElemento('${doc.id}','${doc.data().Cedula}','${doc.data().Nombre}','${doc.data().Telefono}','${doc.data().Ebais}','${doc.data().Direccion}','${doc.data().Recibe}','${doc.data().Fecha}')">  <i class="material-icons">print</i></button></td>
+        </tr>   
+        ` //<- ojo a estas comillas especiales 
+    });
+});
+ }
     
 }
 
